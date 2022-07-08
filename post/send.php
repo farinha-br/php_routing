aa522b94fddfb6dae493d1ea1b6a4b9669897dc0
Carlos <virtualisbol@gmail.com>
2022-07-08 20:05:19 -0300

    Add files via upload

---
+++ post/send.php
@@ -0,0 +1,19 @@
+﻿<!doctype html>
+<html>
+<head lang="pt-br">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<style>
+body { 	margin: 0 auto;	width: 720px; font-family: sans-serif; }
+</style>
+</head>
+<body>
+
+<br/><br/>
+<form method="post" action="?do=posts">
+	<input type="text" id="msg" name="msg" size=30 maxlength=30 value="hello world">
+	<input id="sb" type="submit" value="Send">	
+</form>
+
+</body>
+</html>
---
+++ post/view.php
@@ -0,0 +1,18 @@
+﻿<!doctype html>
+<html>
+<head lang="pt-br">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<style>
+body { 	margin: 0 auto;	width: 720px; font-family: sans-serif; }
+</style>
+</head>
+<body>
+<br/><br/>
+
+﻿Message: <?= $msg ?>
+
+<br/><br/>
+<a href="./">Menu</a>
+</body>
+</html>
