7f0f340bc327120e2f89c4d9608335971e33c75b
Carlos <virtualisbol@gmail.com>
2022-07-08 20:06:13 -0300

    Add files via upload

---
+++ view/pic.php
@@ -0,0 +1,20 @@
+﻿<!doctype html>
+<html>
+<head lang="pt-br">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<style>
+body { 	margin: 0 auto;	width: 720px; font-family: sans-serif; }
+</style>
+</head>
+<body>
+
+<br/><br/>
+
+<img src="assets/sample.svg">
+
+<br/>
+<a href="./">Menu</a>
+
+</body>
+</html>
